a34f61cd3b414ad564d39a2daeb7bcdae1530e3c
Gan Tu <github@tugan.dev>
2022-02-14 21:31:38 -0800

    feat: support redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const uid = useSelector((store) => store.UserReducer.user?.uid);
+  const err = useSelector((store) => store.UserReducer.err);
+  useEffect(() => {
+    if (!uid && !err) {
+      console.log("Fetching user: tugan");
+      dispatch({ type: "FETCH_USER", uid: "tugan" });
+    } else if (!!uid) {
+      console.log("Found user: tugan");
+    }
+  }, [dispatch, uid, err]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <p>UID: {uid}</p>
+      <p>Error: {err}</p>
     </div>
   );
 }
