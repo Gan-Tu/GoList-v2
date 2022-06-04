645b36f44be3e296499a35e1329c136ecbc70e0d
Gan Tu <github@tugan.dev>
2022-06-03 23:22:11 -0700

    improve responsiveness of layout

--- frontend/src/components/CollectionView.js
+++ frontend/src/components/CollectionView.js
@@ -1,48 +1,63 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useCollectionItems } from "../hooks/collections";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ItemView from "./ItemView";
 import { useDispatch } from "react-redux";
 
 function CollectionView() {
   const dispatch = useDispatch();
   const { id } = useParams();
+  const [editMode, toggleEditMode] = useState(false);
   const items = useCollectionItems(id);
 
   useEffect(() => {
     dispatch({ type: "FETCH_COLLECTION", id });
-  }, [id]);
+  }, [dispatch, id]);
 
   return (
-    <div className="p-4 max-w-xl bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <div className="grid text-center space-y-2 mb-6">
+    <div className="p-5 mt-10 min-w-m bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex justify-between items-center text-center h-15 p-4 space-y-2">
         <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
           Web &amp; Mobile Industry
         </h5>
-        <span className="space-x-4">
-          <Link
-            to="/"
-            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-          >
-            Home
-          </Link>
-          <Link
-            to={`/${id}`}
-            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-          >
-            Edit List
-          </Link>
-        </span>
+        <button
+          onClick={() => toggleEditMode(!editMode)}
+          className="text-sm font-medium text-black"
+        >
+          {editMode ? (
+            <p className="w-6 h-6 text-green-800 font-bold">Done</p>
+          ) : (
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+              />
+            </svg>
+          )}
+        </button>
       </div>
-      <div className="flow-root">
-        <ul role="list" className="space-y-4">
+      <div>
+        <ul
+          className={`grid sm:grid-cols-1 ${
+            // Only show 2 column row layout if at least 4 items
+            items?.length >= 4 ? "lg:grid-cols-2" : ""
+          } gap-4`}
+        >
           {items
             ? items.map((id) => (
                 <li
                   key={`item=${id}`}
-                  className="py-3 sm:py-4 border rounded-lg p-4 hover:shadow-md"
+                  className="sm:py-4 border rounded-lg p-4 hover:shadow-md"
                 >
-                  <ItemView id={id} />
+                  <ItemView id={id} editMode={editMode} />
                 </li>
               ))
             : null}
--- frontend/src/components/ItemView.js
+++ frontend/src/components/ItemView.js
@@ -7,7 +7,7 @@ import {
 } from "../hooks/items";
 import { useDispatch } from "react-redux";
 
-function ItemView({ id }) {
+function ItemView({ id, editMode }) {
   const title = useItemTitle(id);
   const snippet = useItemSnippet(id);
   const image = useItemImage(id);
@@ -18,9 +18,54 @@ function ItemView({ id }) {
     dispatch({ type: "FETCH_ITEM", id });
   }, [id]);
 
+  let editGroup = (
+    <div className="pt-2 space-x-4">
+      <a
+        href="#"
+        className="inline-flex items-center text-xs text-blue-500 font-normal hover:underline dark:text-gray-400"
+      >
+        <svg
+          className="w-4 h-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+          />
+        </svg>
+        Edit
+      </a>
+      <a
+        href="#"
+        className="inline-flex items-center text-xs text-blue-500 font-normal hover:underline dark:text-gray-400"
+      >
+        <svg
+          className="w-4 h-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+        </svg>
+        Delete
+      </a>
+    </div>
+  );
+
   return (
     <Fragment>
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-4 ">
         <div className="flex-1 min-w-0">
           <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
             {title}
@@ -37,9 +82,10 @@ function ItemView({ id }) {
           />
         </div>
       </div>
-      <span className="inline-flex items-center text-xs font-normal text-gray-600">
+      <span className="inline-flex justify-between text-xs font-normal text-gray-600">
         {status}
       </span>
+      {editMode ? editGroup : null}
     </Fragment>
   );
 }
