a7f7a9d929a7a32e896e98214d49846c0379030b
Gan Tu <github@tugan.dev>
2022-06-04 00:57:31 -0700

    add breadcrumb

--- frontend/src/components/CollectionView.js
+++ frontend/src/components/CollectionView.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useCollectionItems } from "../hooks/collections";
 import { useEffect, useState } from "react";
 import ItemView from "./ItemView";
@@ -27,11 +27,56 @@ function EditButton({ editMode }) {
   );
 }
 
+function BreadCrumb({ title }) {
+  return (
+    <nav className="flex p-4" aria-label="Breadcrumb">
+      <ol className="inline-flex items-center space-x-1 md:space-x-3">
+        <li className="inline-flex items-center">
+          <Link
+            to="/"
+            className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+          >
+            <svg
+              className="mr-2 w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+            </svg>
+            Home
+          </Link>
+        </li>
+        <li aria-current="page">
+          <div className="flex items-center">
+            <svg
+              className="w-6 h-6 text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+              {title}
+            </span>
+          </div>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
 function CollectionView() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const [editMode, toggleEditMode] = useState(false);
   const items = useCollectionItems(id);
+  const title = "Web & Mobile Industry";
 
   useEffect(() => {
     dispatch({ type: "FETCH_COLLECTION", id });
@@ -39,9 +84,10 @@ function CollectionView() {
 
   return (
     <div className="p-5 mt-10 min-w-m bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <BreadCrumb title={title} />
       <div className="flex justify-between items-center text-center h-15 p-4 space-y-2">
         <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
-          Web &amp; Mobile Industry
+          {title}
         </h5>
         <motion.button
           whileHover={{ scale: 1.1 }}
