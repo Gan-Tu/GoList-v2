db2262b3705124a08420141823a1e1ea4fa1e6cd
Gan Tu <github@tugan.dev>
2022-06-05 00:27:58 -0700

    delete image upload

--- frontend/src/components/Items/ItemModal.js
+++ frontend/src/components/Items/ItemModal.js
@@ -143,31 +143,6 @@ function PreviewButton({ isPreview, setIsPreview }) {
   );
 }
 
-// function ImageUpload() {
-//   return (
-//     <div className="mb-6">
-//       <label
-//         className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-//         htmlFor="image_input"
-//       >
-//         Item Thumbnail
-//       </label>
-//       <input
-//         className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-//         aria-describedby="image_input_help"
-//         id="image_input"
-//         type="file"
-//       />
-//       {/* <p
-//     className="mt-1 text-sm text-gray-500 dark:text-gray-300"
-//     id="image_input_help"
-//   >
-//     SVG, PNG, JPG or GIF (MAX. 800x400px).
-//   </p> */}
-//     </div>
-//   );
-// }
-
 export default function ItemModal({ itemId, isOpen, onClose }) {
   const title = useItemTitle(itemId);
   const snippet = useItemSnippet(itemId);
