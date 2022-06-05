7315bbcb92004c35ca5e191914c4e28658bd2c39
Gan Tu <github@tugan.dev>
2022-06-05 16:57:20 -0700

    delete unused comment

--- frontend/src/state/redux/collections/index.js
+++ frontend/src/state/redux/collections/index.js
@@ -15,7 +15,6 @@
 import { call, put, takeEvery } from "redux-saga/effects";
 
 function* fetchCollection({ id }) {
-  // const resp = yield call(fetch, `http://localhost:8080/collections?username=${username}&_limit=1`);
   const resp = yield call(fetch, `http://localhost:8080/collections/${id}`);
   if (!resp.ok) {
     console.error(resp.statusText);
