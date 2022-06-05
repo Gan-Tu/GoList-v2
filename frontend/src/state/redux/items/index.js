515dabcf396621a7ed03a3daac6086b5c702efe6
Gan Tu <github@tugan.dev>
2022-06-05 14:52:34 -0700

    unremove unused comment

--- frontend/src/state/redux/items/index.js
+++ frontend/src/state/redux/items/index.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// import fetch from "node-fetch";
 import { call, put, select, takeEvery, takeLatest } from "redux-saga/effects";
 
 function* fetchItem({ id }) {
