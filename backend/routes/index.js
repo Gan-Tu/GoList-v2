7c80bd5f1b00668d2148794132f1d0ca373aa1a9
Gan Tu <github@tugan.dev>
2022-06-06 23:56:35 -0700

    implement get metadata

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,12 +12,38 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const axios = require("axios").default;
+const cheerio = require("cheerio");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* GET site metadata. */
+router.post("/getMetadata", function (req, res, next) {
+  axios
+    .get(req.body.url)
+    .then((resp) => {
+      const $ = cheerio.load(resp.data);
+      res.json({
+        title: $("title").text(),
+        description: $("meta[name='description']").attr("content"),
+        ogTitle: $("meta[property='og:title']").attr("content"),
+        ogSiteName: $("meta[property='og:site_name']").attr("content"),
+        ogDescription: $("meta[property='og:description']").attr("content"),
+        ogImage: $("meta[property='og:image']").attr("content"),
+        twitterTitle: $("meta[name='twitter:title']").attr("content"),
+        twitterSite: $("meta[name='twitter:site']").attr("content"),
+        twitterDescription: $("meta[name='twitter:description']").attr(
+          "content"
+        ),
+        twitterImage: $("meta[name='twitter:image']").attr("content"),
+      });
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
