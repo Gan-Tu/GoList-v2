b0c99c99cebcec44c9318d9825cc60af6099ac11
Gan Tu <github@tugan.dev>
2022-06-11 14:53:36 -0400

    remove redundant css

--- frontend/src/components/CreateFlow.js
+++ frontend/src/components/CreateFlow.js
@@ -74,7 +74,7 @@ export default function CreateFlow({ collectionId, isOpen, onClose }) {
           <div className="mt-6">
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center"
+              className="text-white bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center"
               disabled={isLoading}
               onClick={onCreate}
             >
