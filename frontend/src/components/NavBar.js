3f492d4d6e3ed35e65842f85d7f4ab2645904646
Gan Tu <github@tugan.dev>
2022-07-09 20:09:46 -0400

    add sign in logo placeholder

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -12,15 +12,107 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import toast from "react-hot-toast";
+import { motion } from "framer-motion";
+import Modal from "./Utilities/Modal";
+import { useState } from "react";
 
-export default function NavBar() {
+function LoginModal({ isOpen, onClose }) {
+  return (
+    <motion.div layout>
+      <Modal title={"Sign In"} isOpen={isOpen} onClose={onClose}>
+        <div className="grid grid-cols-1 justify-items-center">
+          <ul className="my-4 space-y-3">
+            <li>
+              <button className="flex px-5 py-3 text-sm text-gray-900 bg-gray-50 rounded-lg">
+                <img
+                  src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                  className="mr-3 h-5 "
+                  alt="Google Logo"
+                />
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Sign in with Google
+                </span>
+              </button>
+            </li>
+            <li>
+              <button className="flex px-5 py-3 text-sm text-white bg-facebook-blue rounded-lg">
+                <img
+                  src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/facebook.svg"
+                  className="mr-3 h-5 "
+                  alt="Facebook Logo"
+                />
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Sign in with Facebook
+                </span>
+              </button>
+            </li>
+            <li>
+              <button className="flex px-5 py-3 text-sm text-white bg-twitter-blue rounded-lg">
+                <img
+                  src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/twitter.svg"
+                  className="mr-3 h-5 "
+                  alt="Twitter Logo"
+                />
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Sign in with Twitter
+                </span>
+              </button>
+            </li>
+            <li>
+              <button className="flex px-5 py-3 text-sm text-white bg-github-black rounded-lg">
+                <img
+                  src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/github.svg"
+                  className="mr-3 h-5 "
+                  alt="GitHub Logo"
+                />
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Sign in with GitHub
+                </span>
+              </button>
+            </li>
+            <li>
+              <button className="flex px-5 py-3 text-sm text-white bg-google-red rounded-lg">
+                <img
+                  src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/mail.svg"
+                  className="mr-3 h-5 "
+                  alt="Mail Logo"
+                />
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Sign in with Email
+                </span>
+              </button>
+            </li>
+            <li>
+              <button className="flex px-5 py-3 text-sm text-white bg-google-yellow rounded-lg">
+                <img
+                  src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/anonymous.png"
+                  className="mr-3 h-5 "
+                  alt="Anonymous Logo"
+                />
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Sign in as Guest
+                </span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </Modal>
+    </motion.div>
+  );
+}
+
+function NavBar() {
+  const [signInModalOpen, setSignInModalOpen] = useState(false);
   const onLogin = () => {
-    toast.error("Login is not implemented yet");
+    setSignInModalOpen(true);
   };
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-800">
+      <LoginModal
+        isOpen={signInModalOpen}
+        onClose={() => setSignInModalOpen(false)}
+      />
       <div className="flex flex-wrap justify-between items-center m-4 max-w-screen-xl px-6 py-2.5">
         <a type="button" href="/" className="flex items-center">
           <img
@@ -41,3 +133,5 @@ export default function NavBar() {
     </nav>
   );
 }
+
+export default NavBar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitter-blue": "#1DA1F2",
+        "google-blue": "#4285F4",
+        "google-red": "#DB4437",
+        "google-yellow": "#F4B400",
+        "google-green": "#0F9D58",
+        "facebook-blue": "#4267B2",
+        "github-black": "#171515",
+      },
+    },
   },
-  plugins: [
-    require('flowbite/plugin'),
-    require('@tailwindcss/line-clamp'),
-  ],
-}
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/line-clamp")],
+};
